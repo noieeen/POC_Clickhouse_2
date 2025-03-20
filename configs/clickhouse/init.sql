cb43e41d29fbd9d3f93ffd646a7fa2ebba3c4729
Tharin <tharin.ch@chococrm.com>
2025-03-20 23:08:19 +0700

    update

--- configs/clickhouse/init.sql
+++ configs/clickhouse/init.sql
@@ -1,50 +1,29 @@
--- -- Create database for logs
--- CREATE DATABASE IF NOT EXISTS logs;
--- 
--- -- Create table for application logs
--- CREATE TABLE IF NOT EXISTS logs.logs
--- (
---     timestamp DateTime64(9) DEFAULT now(),
---     trace_id String,
---     span_id String,
---     severity_text String,
---     severity_number UInt8,
---     service_name String,
---     service_version String,
---     body String,
---     resource_attributes Map(String, String),
---     attributes Map(String, String)
---     )
---     ENGINE = MergeTree()
---     ORDER BY (timestamp, service_name)
---     TTL timestamp + INTERVAL 30 DAY;
--- 
--- -- Create materialized view for quick access to error logs
--- CREATE MATERIALIZED VIEW IF NOT EXISTS logs.error_logs
--- ENGINE = MergeTree()
--- ORDER BY (timestamp, service_name)
--- TTL timestamp + INTERVAL 30 DAY
--- AS SELECT *
---    FROM logs.logs
---    WHERE severity_text IN ('ERROR', 'FATAL');
-
 -- Create database for logs
 CREATE DATABASE IF NOT EXISTS logs;
+DROP TABLE IF EXISTS logs.logs;
 
 -- Create table for application logs
 CREATE TABLE IF NOT EXISTS logs.logs
 (
-    timestamp DateTime64(9) DEFAULT now(),
-    trace_id String,
-    span_id String,
-    severity_text String,
+    timestamp DateTime,
+    observed_timestamp DateTime,
     severity_number UInt8,
-    service_name String,
-    service_version String,
-    body String,
-    resource_attributes Map(String, String),
-    attributes Map(String, String)
+    severity_text String,
+    message String,
+    attributes Nested(
+    key String,
+    value String
+),
+    dropped_attributes_count UInt32,
+    flags UInt8,
+    resources Nested(
+                        key String,
+                        value String
+                    ),
+    scope_name String,
+    span_id String,
+    trace_id String,
+    source_type String
     )
     ENGINE = MergeTree()
-    ORDER BY (timestamp, service_name)
-    TTL toDateTime(timestamp) + INTERVAL 30 DAY;
+    ORDER BY timestamp;
