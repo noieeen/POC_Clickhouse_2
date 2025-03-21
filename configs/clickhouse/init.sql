2afd37bba608b882162a66974ace0e5ecbfa5f42
Tharin <tharin.ch@chococrm.com>
2025-03-21 19:52:14 +0700

    update

--- configs/clickhouse/init.sql
+++ configs/clickhouse/init.sql
@@ -1,10 +1,11 @@
 -- Create database for logs
-CREATE
-DATABASE IF NOT EXISTS default;
+CREATE DATABASE IF NOT EXISTS default;
 
-CREATE
-DATABASE IF NOT EXISTS logs;
-
+CREATE DATABASE IF NOT EXISTS logs;
+       
+    SET enable_json_type = 1;
+    SET max_insert_block_size = 1048576;
+        
 DROP TABLE IF EXISTS logs.logs;
 DROP TABLE IF EXISTS logs.dummy;
 DROP TABLE IF EXISTS logs.otel_traces;
@@ -12,7 +13,7 @@ DROP TABLE IF EXISTS logs.otel_traces;
 -- -- LOGS      
 DROP TABLE IF EXISTS default.otel_logs;
 -- -- TRACES
-DROP TABLE IF EXISTS default.traces;
+DROP TABLE IF EXISTS default.otel_traces;
 
 CREATE TABLE IF NOT EXISTS default.otel_logs
 (
@@ -33,16 +34,17 @@ CREATE TABLE IF NOT EXISTS default.otel_logs
 PARTITION BY toYYYYMM(Timestamp)
 ORDER BY (Timestamp, ServiceName, SeverityNumber);
 
-CREATE TABLE IF NOT EXISTS default.traces
+CREATE TABLE IF NOT EXISTS logs.traces
 (
+    id UInt64,
     Timestamp         DateTime64(9) DEFAULT now(),
     ObservedTimestamp TIMESTAMP DEFAULT NULL,
     Body              String,
-    LogResources      Map(String, String),
-    LogAttributes     Map(String, String)
+--     LogResources      Map(String, String),
+--     LogAttributes     Map(String, String)
     ) ENGINE = MergeTree()
-    PARTITION BY toYYYYMM(Timestamp)
-    ORDER BY (Timestamp);
+--     PARTITION BY toYYYYMM(Timestamp)
+    ORDER BY (id,Timestamp);
 
 
 -- CREATE TABLE IF NOT EXISTS default.otel_traces
@@ -70,9 +72,25 @@ CREATE TABLE IF NOT EXISTS default.traces
 
 CREATE TABLE logs.dummy
 (
+    Id UInt64,
     Timestamp         DateTime64(9) DEFAULT now(),
-    TraceId               String,
+    TraceId              String,
     Body                 String
 ) ENGINE = MergeTree()
 PARTITION BY toYYYYMM(Timestamp)
-ORDER BY (Timestamp);
+ORDER BY (Id, Timestamp);
+
+CREATE TABLE default.otel_traces (
+                                     Timestamp DateTime64(9) DEFAULT now(),
+                                     trace_id String,
+                                     span_id String,
+                                     parent_span_id String,
+                                     name String,
+                                     kind UInt8,
+                                     start_time DateTime64(9),
+                                     end_time DateTime64(9),
+                                     attributes JSON,
+                                     status_code UInt8,
+                                     status_message String
+) ENGINE = MergeTree()
+ORDER BY (Timestamp, trace_id, span_id);
